ce4887eb0be446d7227b6a0e8aa0086a6fafc7ae
Trung Nguyen <trung.nguyen@polytechnique.org>
2015-04-29 00:17:49 +0200

    [feoct_monit] Success generation of .tpc (tree_print_code)

--- feoct/feoct.cc
+++ feoct/feoct.cc
@@ -5,6 +5,7 @@
 #include "feoct_monit.hh"
 #include "feoct.hh"
 #include "ov-usr-fcn.h"
+#include "pt-all.h"
 
 void feoct_init (){
   printf("------------INIT MAIN LOOP--------------------- \n");
@@ -13,12 +14,24 @@ void feoct_init (){
 void feoct_record (octave_user_function *fcn){
   printf("------------PARSER: %s\n", fcn->name().c_str());
 
-  const char * monitFilename = (fcn->name()+".feoct").c_str();
+  FILE *dumpfile;
+  if(1){
+    const char * monitFilename = (fcn->name()+".feoct").c_str();
+    dumpfile = fopen (monitFilename, "w");
+    tree_monit td (dumpfile);
+    fcn->accept (td);
+  }
+  else{
+    const char * monitFilename = (fcn->name()+".tpc").c_str();
+    dumpfile = fopen (monitFilename, "w");
 
-  FILE * f = fopen (monitFilename, "w");
-  tree_monit td (f);
-  fcn->accept (td);
-  fclose (f);
+    std::ostringstream buf;
+    tree_print_code tpc (buf);
+    fcn->accept (tpc);
+    fprintf (dumpfile, "%s\n", (buf.str()).c_str());
+    //cout<<buf.str()<<endl;
+  }
+  fclose (dumpfile);
 };
 
 void feoct_end (){
